cd146abd18e00c72e1e368faea2a0d8f42cb77a1
Quinten Clause <quintenclause@hotmail.com>
2024-03-17 09:19:01 +0100

    mv fillrect back to template

--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import data from './data.js';
 import pointer from './pointer.js';
 import resize from './resize.js';
 import sound from './sound.js';
-import fillrect from './fillrect.js';
 
 exports.components = Object.freeze({
     data,
     pointer,
     resize,
     sound,
-    fillrect
 });
